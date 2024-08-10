c215836fe3180fcdab89f8e19d3fc7982db61662
RussianCoderRnD <stalkerolegator@yandex.ru>
2024-08-10 18:19:24 +0300

    finish10082024

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,26 @@
 //----------------- БИБЛИОТЕКИ END-----------------------
 
 //------------ СОЗДАНИЕ ЭКЗЕМПЛЯРА START ----------------
-IPAddress local_ip(192, 168, 4, 11); /* Локальный IP-адрес */
-IPAddress gateway(192, 168, 4, 11);  /* Шлюз */
+PressureSensor pressureSensor(PRESSURE_SENSOR_PIN, 7.2); /* Создание объекта датчика давления */
+
+PressureController pressureController(Pin_PUMP); /* Создание объекта с номером ПИНа реле */
+
+GyverOLED<SSD1306_128x64, OLED_BUFFER> oled;
+
+IPAddress local_ip(192, 168, 4, 10); /* Локальный IP-адрес */
+IPAddress gateway(192, 168, 4, 10);  /* Шлюз */
 IPAddress subnet(255, 255, 255, 0);  /* Маска подсети */
 
 ESP8266WebServer server(80); /* Создание экземпляра веб-сервера на порту 80 */
 
-GyverOLED<SSD1306_128x64, OLED_BUFFER> oled;
 EEPROMHandler eepromHandler;
+
+// Создание экземпляра класса MyDisplay
+MyDisplay MyDis(oled);
+MyDisplayString MyDisS(oled);
+
+// Датчики на D2
+MicroDS18B20<Pin_DS18B20> DS18B20_sensor;
 //------------- СОЗДАНИЕ ЭКЗЕМПЛЯРА END -----------------
 
 //----------------- CONSTANTS START ---------------------
@@ -26,10 +38,6 @@ const char *password = "19781503"; /* Пароль сети */
 void setBright();   // Функция для ограничения температуры и установки флага EEPROM
 void checkEEPROM(); // Функция для проверки и записи данных в EEPROM
 
-// Создание экземпляра класса MyDisplay
-MyDisplay MyDis(oled);
-MyDisplayString MyDisS(oled);
-
 void setup()
 {
   Serial.begin(MONITOR_SPEED); /* Открываем порт для связи с ПК */
@@ -44,19 +52,48 @@ void setup()
 
   eepromHandler.begin(32); /* Инициализация EEPROM с размером 512 байт */
 
+  hysteresis = eepromHandler.readFloat(6); /* Чтение значения из EEPROM */
+  count = eepromHandler.readFloat(0);      /* Чтение значения из EEPROM */
+
   oled.clear();  // очистить дисплей (или буфер)
   oled.update(); // обновить. Только для режима с буфером! OLED_BUFFER
+  MyDisS.myDisplayString(0, 0, 0, "DS18B20: ");
+  MyDisS.myDisplayString(0, 10, 0, "Pressure: ");
+  MyDisS.myDisplayString(0, 24, 0, "Count: ");
+  MyDisS.myDisplayString(0, 35, 0, "Hysteresis: ");
 }
 
 void loop()
 {
-  checkEEPROM(); /* Проверка и запись в EEPROM */
-  if (millis() - tmr >= MY_PERIOD)
+  checkEEPROM(); /* Проверка и запись в EEPROM */ //
+                                                  // boolean temps = digitalRead(pressureController.getRelayPin()) ? "ON" : "OFF";
+
+  if (millis() - tmr >= 1000)
   {
     tmr = millis(); /* Сброс таймера */
-    MyDis.myDisplay(0, 5, 0, 1, "Влажность: ");
-    MyDis.myDisplay(0, 20, 0, 2, "Температура: ");
-    MyDis.myDisplay(0, 35, 0, 3, "Порог влажности: ");
+
+    pressure = pressureSensor.getPressure(); /* Получение давления */
+
+    pressureController.setParameters(count, hysteresis);
+
+    // просто выводим температуру первого датчика
+    DS18B20_temperature = DS18B20_sensor.getTemp();
+
+    // проверяем успешность чтения и выводим
+    if (DS18B20_sensor.readTemp())
+      DS18B20_sensor_status = true;
+    else
+      DS18B20_sensor_status = false;
+
+    DS18B20_sensor.requestTemp();
+
+    /* Обновление состояния реле на основе текущего давления */
+    pressureController.update(pressure, onoff);
+
+    MyDisS.myDisplayString(80, 0, 0, (String)DS18B20_temperature);
+    MyDisS.myDisplayString(80, 10, 0, (String)pressure);
+    MyDisS.myDisplayString(80, 24, 0, (String)count);
+    MyDisS.myDisplayString(80, 35, 0, (String)hysteresis);
   }
   server.handleClient(); /* Обработка клиентских запросов */
   ArduinoOTA.handle();
@@ -72,16 +109,17 @@ void handleRoot()
 //========== Обработчик запроса "/res"
 void handleRes()
 {
+  boolean RelayOnOff = digitalRead(Pin_PUMP);
   /* Создаем JSON объект */
   StaticJsonDocument<200> jsonDoc;
   /* Добавляем переменные в JSON объект */
-  jsonDoc["PR"] = 4.1;                     // pressure;
-  jsonDoc["COUNT"] = 2.2;                  // count;
-  jsonDoc["ONOFF"] = true;                 // onoff;
-  jsonDoc["HYSTERESIS"] = 0.3;             // hysteresis;
-  jsonDoc["RelayOnOff"] = true;            // RelayOnOff;
-  jsonDoc["DS18B20_sensor_status"] = true; // DS18B20_sensor_status;
-  jsonDoc["DS18B20_temperature"] = 24.6;   // DS18B20_temperature;
+  jsonDoc["PR"] = String(pressure,2);                     // pressure;
+  jsonDoc["COUNT"] = String(count,2);                 // count;
+  jsonDoc["ONOFF"] = onoff;                 // onoff;
+  jsonDoc["HYSTERESIS"] = String(hysteresis,2);             // hysteresis;
+  jsonDoc["RelayOnOff"] = RelayOnOff;            // RelayOnOff;
+  jsonDoc["DS18B20_sensor_status"] = DS18B20_sensor_status; // DS18B20_sensor_status;
+  jsonDoc["DS18B20_temperature"] =  DS18B20_temperature;   // DS18B20_temperature;
 
   /* Преобразуем JSON объект в строку */
   String jsonString;
@@ -114,11 +152,39 @@ void handleOnOff()
 //========== Обработчик запроса "/hysteresis"
 void handleHysteresis()
 {
+  String message = "";
+  if (server.hasArg("hysteresis"))
+  {
+    hysteresis = server.arg("hysteresis").toFloat(); // Получаем значение параметра hysteresis и конвертируем в int
+    setBright();
+    message = "The hysteresis value is set: " + String(hysteresis);
+    Serial.println("The hysteresis value is set: " + String(hysteresis));
+  }
+  else
+  {
+    message = "Error: The hysteresis parameter is missing or the format is incorrect";
+    Serial.println("Error: The hysteresis parameter is missing or the format is incorrect");
+  }
+  server.send(200, "text/plain", message);
 }
 
 //========== Обработчик запроса "/count"
 void handleCount()
 {
+  String message = "";
+  if (server.hasArg("count"))
+  {
+    count = server.arg("count").toFloat(); /* Получаем значение параметра count и конвертируем в float */
+    setBright();
+    message = "The count value is set: " + String(count);
+    Serial.println("The count value is set: " + String(count));
+  }
+  else
+  {
+    message = "Error: The count parameter is missing or the format is incorrect";
+    Serial.println("Error: The count parameter is missing or the format is incorrect");
+  }
+  server.send(200, "text/plain", message);
 }
 
 //====================setBright==========================
